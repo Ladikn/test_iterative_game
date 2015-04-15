105603d54d2a9dba6bf050771860f1e2073945e0
Ladikn <wyz127@gmail.com>
2015-04-15 14:46:52 -0400

    Added save/load features
    Missed the js

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-var gold = 0, workers = 0;
+var kingdom = {
+	gold: 0, 
+	workers: 0
+}
+
+window.setInterval(function(){
+	goldClick(kingdom.workers);
+}, 1000);
+
+function saveGame() {
+	localStorage.setItem("save", JSON.stringify(kingdom));
+	alert("Game Saved");
+}
+
+function loadGame() {
+	var save = JSON.parse(localStorage.getItem("save"));
+	if (typeof save !== "undefined") kingdom = save;
+	refreshPage();
+}
 
 function goldClick(x) {
-    gold = gold + x;
-    $("#goldCount").text(gold);
+    kingdom.gold = kingdom.gold + x;
+    refreshPage();
 };
 
 function buyWorker() {
-	var workerCost = Math.floor(10 * Math.pow(1.1, workers));
-	if (gold >= workerCost) {
-		workers = workers + 1;
-		gold = gold - workerCost;
-		$("#goldCount").text(gold);
-		$("#workerCount").text(workers);
+	var workerCost = Math.floor(10 * Math.pow(1.1, kingdom.workers));
+	if (kingdom.gold >= workerCost) {
+		kingdom.workers = kingdom.workers + 1;
+		kingdom.gold = kingdom.gold - workerCost;
 	};
-	var nextCost = Math.floor(10 * Math.pow(1.1, workers));
+	var nextCost = Math.floor(10 * Math.pow(1.1, kingdom.workers));
 	$("#workerCost").text(nextCost);
+	refreshPage();
+};
+
+function refreshPage() {
+	//Kindgom Attributes
+	$("#goldCount").text(kingdom.gold);
+	$("#workerCount").text(kingdom.workers);
+
+	//Costs
+	$("#workerCost").text(Math.floor(10 * Math.pow(1.1, kingdom.workers)));
 }
